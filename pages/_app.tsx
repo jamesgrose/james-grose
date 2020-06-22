c1fcbd11cac50756e2b95386c9caf4728b878432
James Grose <grose.james@gmail.com>
2020-06-22 13:29:05 +1000

    Report web vitals to Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,16 @@ import "../styles/index.css";
 
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
+export function reportWebVitals({ id, name, label, value }): void {
+  gtag.event({
+    action: name,
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    label: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    value: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
